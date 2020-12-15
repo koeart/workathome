423f250cb2e43eaa08edcf41d8ffb5eef64b610d
koeart <koeart@users.noreply.github.com>
2020-12-15 00:03:49 +0000

    Deploying to gh-pages from master @ 1267d62d0cf0a3c80628b922b661c15bb4ab4c03 🚀

---
+++ tilesets/c3_deko.tsx
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="c3_deko" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <image source="c3_deko.png" trans="000000" width="512" height="2048"/>
+ <tile id="8">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="458">
+  <animation>
+   <frame tileid="266" duration="230"/>
+   <frame tileid="304" duration="230"/>
+   <frame tileid="309" duration="230"/>
+   <frame tileid="314" duration="230"/>
+   <frame tileid="352" duration="230"/>
+   <frame tileid="357" duration="230"/>
+   <frame tileid="362" duration="230"/>
+   <frame tileid="400" duration="230"/>
+   <frame tileid="405" duration="230"/>
+   <frame tileid="410" duration="230"/>
+   <frame tileid="448" duration="230"/>
+  </animation>
+ </tile>
+ <tile id="462">
+  <animation>
+   <frame tileid="270" duration="230"/>
+   <frame tileid="308" duration="230"/>
+   <frame tileid="313" duration="230"/>
+   <frame tileid="318" duration="230"/>
+   <frame tileid="356" duration="230"/>
+   <frame tileid="361" duration="230"/>
+   <frame tileid="366" duration="230"/>
+   <frame tileid="404" duration="230"/>
+   <frame tileid="409" duration="230"/>
+   <frame tileid="414" duration="230"/>
+   <frame tileid="452" duration="230"/>
+  </animation>
+ </tile>
+ <tile id="474">
+  <animation>
+   <frame tileid="282" duration="230"/>
+   <frame tileid="320" duration="230"/>
+   <frame tileid="325" duration="230"/>
+   <frame tileid="330" duration="230"/>
+   <frame tileid="368" duration="230"/>
+   <frame tileid="373" duration="230"/>
+   <frame tileid="378" duration="230"/>
+   <frame tileid="416" duration="230"/>
+   <frame tileid="421" duration="230"/>
+   <frame tileid="426" duration="230"/>
+   <frame tileid="464" duration="230"/>
+  </animation>
+ </tile>
+ <tile id="475">
+  <animation>
+   <frame tileid="283" duration="230"/>
+   <frame tileid="321" duration="230"/>
+   <frame tileid="326" duration="230"/>
+   <frame tileid="331" duration="230"/>
+   <frame tileid="369" duration="230"/>
+   <frame tileid="374" duration="230"/>
+   <frame tileid="379" duration="230"/>
+   <frame tileid="417" duration="230"/>
+   <frame tileid="422" duration="230"/>
+   <frame tileid="427" duration="230"/>
+   <frame tileid="465" duration="230"/>
+  </animation>
+ </tile>
+ <tile id="476">
+  <animation>
+   <frame tileid="284" duration="230"/>
+   <frame tileid="322" duration="230"/>
+   <frame tileid="327" duration="230"/>
+   <frame tileid="332" duration="230"/>
+   <frame tileid="370" duration="230"/>
+   <frame tileid="375" duration="230"/>
+   <frame tileid="380" duration="230"/>
+   <frame tileid="418" duration="230"/>
+   <frame tileid="423" duration="230"/>
+   <frame tileid="428" duration="230"/>
+   <frame tileid="466" duration="230"/>
+  </animation>
+ </tile>
+ <tile id="477">
+  <animation>
+   <frame tileid="285" duration="230"/>
+   <frame tileid="323" duration="230"/>
+   <frame tileid="328" duration="230"/>
+   <frame tileid="333" duration="230"/>
+   <frame tileid="371" duration="230"/>
+   <frame tileid="376" duration="230"/>
+   <frame tileid="381" duration="230"/>
+   <frame tileid="419" duration="230"/>
+   <frame tileid="424" duration="230"/>
+   <frame tileid="429" duration="230"/>
+   <frame tileid="467" duration="230"/>
+  </animation>
+ </tile>
+ <tile id="478">
+  <animation>
+   <frame tileid="286" duration="230"/>
+   <frame tileid="324" duration="230"/>
+   <frame tileid="329" duration="230"/>
+   <frame tileid="334" duration="230"/>
+   <frame tileid="372" duration="230"/>
+   <frame tileid="377" duration="230"/>
+   <frame tileid="382" duration="230"/>
+   <frame tileid="420" duration="230"/>
+   <frame tileid="425" duration="230"/>
+   <frame tileid="430" duration="230"/>
+   <frame tileid="468" duration="230"/>
+  </animation>
+ </tile>
+</tileset>
